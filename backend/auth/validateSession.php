5aa0932ffff289f1d70d1c5dbaf7545fa7a89fa0
AlperenBasali <alperenbasali3@gmail.com>
2025-07-21 15:19:32 +0300

    .

--- backend/auth/validateSession.php
+++ backend/auth/validateSession.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Eğer session'da admin oturumu varsa, 200 dön
 if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
